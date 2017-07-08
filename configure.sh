aa933c3a209fcbdfc7e7294050cc0c0f6bf05ccc
Torsten Fink <torstenfink@posteo.de>
2017-07-08 18:24:53 +0200

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -12,7 +12,7 @@ locale-gen
 echo "KEYMAP=de-latin1" > /etc/vconsole.conf
 echo "tfink-laptop" > /etc/hostname
 
-# add sd-lvm2 to initramfs
+# add sd-lvm2 to initramfsnt/boot
 nano /etc/mkinitcpio.conf
 #sed ...
 mkinitcpio -p linux
@@ -20,5 +20,18 @@ mkinitcpio -p linux
 # root password
 passwd
 
+pacman -S intel-ucode
+
 # boot loader
 bootctl install
+nano /boot/loader/loader.conf
+#default arch
+#timeout 0
+#editor 0
+
+nano /boot/loader/entries/arch-lvm.conf
+#title   Arch Linux
+#linux   /vmlinuz-linux
+#initrd  /intel-ucode.img
+#initrd  /initramfs-linux.img
+#options root=/dev/mapper/VolGroup00-lvol_root rw
